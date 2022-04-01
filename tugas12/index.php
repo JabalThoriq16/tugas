b53ee9a6ac4ff75216899a826a7516bdfd6b89c4
JabalThoriq16 <riqda.corp@gmail.com>
2022-04-01 21:51:48 +0700

    tugas 12 vue js

---
+++ tugas12/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Component Veu.js</title>
+</head>
+<body>
+    
+    <div id="app" v-if = "status == 'tampilkan'">
+        {{ message }}<br><br>
+        <label v-for="user in user">Name : {{user.name}}, Umur: {{user.age}}<br></label>
+    </div>
+
+
+    <script src="https://unpkg.com/vue@3"></script>
+    <script type="text/javascript">
+        var app = new Vue({
+            el:'#app',
+            data:{
+                massage: 'Hello Vue!',
+                status: 'tampilkan',
+                user:[
+                    {
+                        name:'Jabal',
+                        age:'20'
+                    },
+                    {
+                        name:'thoriq',
+                        age:'21'
+                    },
+                    {
+                        name:'jabar',
+                        age:'22'
+                    }
+                ]
+            }
+        })
+    </script>
+</body>
+</html>

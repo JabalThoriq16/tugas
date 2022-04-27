0f8b5644fa1fc75fa6fc58b9f234511e252e56f3
JabalThoriq16 <riqda.corp@gmail.com>
2022-04-27 11:48:25 +0700

    tugas31

--- tugas15/app/Http/Controllers/HomeController.php
+++ tugas15/app/Http/Controllers/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+// use DB;
 use Illuminate\Http\Request;
-use Illuminate\Http\DB;
+use DB;
 use App\Models\Member;
 use App\Models\Book;
 use App\Models\Publisher;
@@ -29,9 +30,33 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // $member = Member::with('user')->get();
-        // $book = Book::with('publisher')->get();
-        // $publisher = Publisher::all();
-        return view('home');
+       $total_member =Member::count();
+       $total_book =Book::count();
+       $total_tarnsaction =Transaction::whereMonth('date_start' , date('m')) ->count();
+       $total_publisher =Publisher::count();
+
+       $data_donut = Book::select(DB::raw("COUNT(publisher_id) as total"))
+       ->groupBy('publisher_id')
+       ->orderBy('publisher_id','asc')
+       ->pluck('total');
+
+       $label_donut= Publisher::orderBy('publishers.id', 'asc')->join('books','books.publisher_id', '=', 'publishers.id')->groupBy('publishers.name')->pluck('publishers.name');
+       $label_bar= ['Trancation'];
+       $data_label=[];
+
+       foreach($label_bar as $key => $value){
+        $data_bar[$key]['lebel']=$label_bar[$key];
+        $data_bar[$key]['backgroundColor'] = 'rgba(60,141,188,0,9)';
+        $data_month =[];
+
+        foreach (range(1,12) as $month){
+            $data_month[]=Transaction::select(DB::raw("COUNT(*) as total"))->whereMonth('date_start', $month)->first()->total;
+        }
+        $data_bar[$key]['data']=$data_month;
+       }
+       
+
+    //    return $data_bar;
+        return view('home', compact('total_member','total_book','total_tarnsaction','total_publisher','data_donut','label_donut','label_bar','data_label','data_bar','label_bar'));
     }
 }
--- tugas15/resources/views/home.blade.php
+++ tugas15/resources/views/home.blade.php
@@ -1,6 +1,201 @@
 @extends('layouts.layouts')
 @section('header', 'Home')
 
+@section('css')
+
+@endsection
+
 @section('content')
-    <p>selamat datang, ini Dashboard</p>
+    <div class="row">
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $total_member }}</h3>
+                    <p>Members</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $total_book }}</h3>
+                    <p>Total Book</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $total_tarnsaction }}</h3>
+                    <p>Total Tarnsaction</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $total_publisher }}</h3>
+                    <p>Total Publisher</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card card-danger">
+                <div class="card-header">
+                    <h3 class="card-title">Donut Chart</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="chartjs-size-monitor">
+                        <div class="chartjs-size-monitor-expand">
+                            <div class=""></div>
+                        </div>
+                        <div class="chartjs-size-monitor-shrink">
+                            <div class=""></div>
+                        </div>
+                    </div>
+                    <canvas id="donutChart"
+                        style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 358px;"
+                        width="358" height="250" class="chartjs-render-monitor"></canvas>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-6">
+
+
+            <div class="card card-success">
+                <div class="card-header">
+                    <h3 class="card-title">Bar Chart</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="chart">
+                        <div class="chartjs-size-monitor">
+                            <div class="chartjs-size-monitor-expand">
+                                <div class=""></div>
+                            </div>
+                            <div class="chartjs-size-monitor-shrink">
+                                <div class=""></div>
+                            </div>
+                        </div>
+                        <canvas id="barChart"
+                            style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 358px;"
+                            width="358" height="250" class="chartjs-render-monitor"></canvas>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+@endsection
+
+@section('js')
+    <script src="{{ asset('asset/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('asset/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('asset/plugins/chart.js/Chart.min.js') }}"></script>
+    <script>
+
+        var label_donut = '{!! json_encode($label_donut)!!}';
+        var data_donut = '{!! json_encode($data_donut)!!}';
+        var data_bar = '{!! json_encode($data_bar)!!}';
+        console.log(data_bar);
+        $(function(){
+             //- DONUT CHART -
+            //-------------
+            // Get context with jQuery - using jQuery's .get() method.
+            var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+            var donutData        = {
+            labels:JSON.parse(label_donut),
+            datasets: [
+                {
+                data:JSON.parse(data_donut),
+                backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de','#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+                }
+            ]
+            }
+            var donutOptions     = {
+            maintainAspectRatio : false,
+            responsive : true,
+            }
+            //Create pie or douhnut chart
+            // You can switch between pie and douhnut using the method below.
+            new Chart(donutChartCanvas, {
+            type: 'doughnut',
+            data: donutData,
+            options: donutOptions
+            })
+
+            //-------------
+            //- BAR CHART -
+            //-------------
+            var areaChartData = {
+            labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: data_bar
+            }
+            var barChartCanvas = $('#barChart').get(0).getContext('2d')
+            var barChartData = $.extend(true, {}, areaChartData)
+            var temp0 = areaChartData.datasets[0]
+            var temp1 = areaChartData.datasets[1]
+            barChartData.datasets[0] = temp1
+            barChartData.datasets[1] = temp0
+
+            var barChartOptions = {
+            responsive              : true,
+            maintainAspectRatio     : false,
+            datasetFill             : false
+            }
+
+            new Chart(barChartCanvas, {
+            type: 'bar',
+            data: barChartData,
+            options: barChartOptions
+            })
+        })
+    </script>
+    
 @endsection
